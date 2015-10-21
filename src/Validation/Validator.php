b2720944bb5734bd0b653378dd563ab4106ebbcf
Paul Vidal <paul.vidal.lujan@gmail.com>
2015-10-20 21:35:11 -0500

    constuctor added

--- src/Validation/Validator.php
+++ src/Validation/Validator.php
@@ -6,6 +6,18 @@ use Illuminate\Validation\Validator as IlluminateValidator;
 
 class Validator extends IlluminateValidator
 {
+	public function __construct($translator, $data, $rules, $messages, array $customAttributes = array())
+    {
+        // Set custom validation error messages
+        if(!isset($messages['null_or_exists']))
+        {
+            $messages['null_or_exists'] = $translator->get(
+                'daylight-validator::validation.null_or_exists'
+            );
+        }
+        parent::__construct($translator, $data, $rules, $messages, $customAttributes);
+    }
+
     public function validateNullOrExists( $attribute, $value, $parameters )
     {
         if ($value == null)
